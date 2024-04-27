f744f3db27654707fe5aa0ceefde447e3ba5010a
ayush <iamrealglider@gmail.com>
2024-04-27 23:58:39 +0530

    designing faq page

---
+++ faq/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <div className=" h-screen w-full bg-[#F9EEFF] ">
+        <div className="main w-full  h-[350px] ">
+          <div className="w-full h-screen flex justify-center items-center">
+            <div className="min-w-[450px] min-h-[450px]  rounded-lg border-2 bg-white">
+              <h1 className="inline-flex items-center mt-5 ml-5">
+                <img src="/images/icon-star.svg" className="mr-5" alt="" />
+                <span className="text-5xl font-bold">FAQs</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
