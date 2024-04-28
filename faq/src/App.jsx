1bc122881b63204bbe8ab3c9a110b3b2ab117c15
ayush <iamrealglider@gmail.com>
2024-04-29 00:12:01 +0530

    faq responsive site completed

--- faq/src/App.jsx
+++ faq/src/App.jsx
@@ -1,14 +1,100 @@
+import { useState } from "react";
+
 function App() {
+  let [showText, setShowText] = useState({
+    image1: false,
+    image2: false,
+    image3: false,
+  });
+  const toggleText = (imgId) => {
+    setShowText({
+      ...showText,
+      [imgId]: !showText[imgId],
+    });
+    console.log(showText);
+  };
   return (
     <>
       <div className=" h-screen w-full bg-[#F9EEFF] ">
         <div className="main w-full  h-[350px] ">
           <div className="w-full h-screen flex justify-center items-center">
-            <div className="min-w-[450px] min-h-[450px]  rounded-lg border-2 bg-white">
+            <div className="w-[450px] min-h-fit p-2 rounded-lg border-2 bg-white m-3">
               <h1 className="inline-flex items-center mt-5 ml-5">
                 <img src="/images/icon-star.svg" className="mr-5" alt="" />
                 <span className="text-5xl font-bold">FAQs</span>
               </h1>
+              <div>
+                <h1 className="flex justify-center mt-4 ">
+                  <span className="mr-6 font-semibold hover:text-[#AA32F3] cursor-pointer">
+                    What is Frontend Mentor, and how will it help me?
+                  </span>
+                  <img
+                    src={`/images/icon-${
+                      showText.image1 ? "minus" : "plus"
+                    }.svg`}
+                    className="cursor-pointer"
+                    alt=""
+                    onClick={() => toggleText("image1")}
+                  />
+                </h1>
+                <p
+                  className={`text-left p-2  text-[#8F858F] ${
+                    showText.image1 ? "block" : "hidden"
+                  }`}
+                >
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+                  debitis blanditiis fugit, odio quos animi! Doloribus quis quo
+                  fugiat labore. Lorem ipsum dolor sit..
+                </p>
+              </div>
+              <div>
+                <h1 className="flex justify-center mt-4 ">
+                  <span className="mr-6 font-semibold  hover:text-[#AA32F3] cursor-pointer">
+                    What is Frontend Mentor, and how will it help me?
+                  </span>
+                  <img
+                    src={`/images/icon-${
+                      showText.image2 ? "minus" : "plus"
+                    }.svg`}
+                    className="cursor-pointer"
+                    alt=""
+                    onClick={() => toggleText("image2")}
+                  />
+                </h1>
+                <p
+                  className={`text-left p-2  text-[#8F858F] ${
+                    showText.image2 ? "block" : "hidden"
+                  }`}
+                >
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+                  debitis blanditiis fugit, odio quos animi! Doloribus quis quo
+                  fugiat labore. Lorem ipsum dolor sit..
+                </p>
+              </div>
+              <div className="mb-10">
+                <h1 className="flex justify-center mt-4 cursor-pointer">
+                  <span className="mr-6 font-semibold  hover:text-[#AA32F3]">
+                    What is Frontend Mentor, and how will it help me?
+                  </span>
+                  <img
+                    src={`/images/icon-${
+                      showText.image3 ? "minus" : "plus"
+                    }.svg`}
+                    className="cursor-pointer"
+                    alt=""
+                    onClick={() => toggleText("image3")}
+                  />
+                </h1>
+                <p
+                  className={`text-left p-2  text-[#8F858F] ${
+                    showText.image3 ? "block" : "hidden"
+                  }`}
+                >
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+                  debitis blanditiis fugit, odio quos animi! Doloribus quis quo
+                  fugiat labore. Lorem ipsum dolor sit..
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- newsletter/src/App.jsx
+++ newsletter/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
     <>
       {isTrue === true ? (
         <>
-          <div className="flex justify-center h-screen items-center">
-            <div className="text-3xl w-[350px] ml-6 px-5 h-[350px] bg-white border-2 rounded-lg">
+          <div className="flex justify-center h-screen items-center mt-  ">
+            <div className="text-3xl lg:w-[340px] w-[320px]  pt-28 lg:pt-0  bg-white ml-6 lg:px-5 lg:h-[350px] h-[100vh] border-2 rounded-lg">
               <img
                 src="/images/tick.svg"
                 alt=""
@@ -35,31 +35,34 @@ function App() {
         <>
           <div className="main w-full h-screen  flex items-center justify-center">
             {/* div for making items center vertically and horizontally */}
-            <div className="sec-main w-[700px] h-[500px] bg-white rounded-3xl flex  lg:flex-row justify-between ">
+            <div className="sec-main h-full  flex-col w-fit lg:w-[700px] lg:h-[500px] bg-white rounded-3xl flex  lg:flex-row justify-between ">
               {/* first for text  */}
+              <div className="block lg:hidden">
+                <img className="w-" src="/images/mob.svg" alt="" />
+              </div>
               <div>
                 <div>
-                  <h1 className="text-4xl mb-5 mt-14 ml-16 font-bold">
+                  <h1 className="mb:text-4xl text-3xl mb-3 lg:mb-3 lg:mt-14 ml-16 font-bold">
                     Stay updated!
                   </h1>
-                  <p className="  ml-10">
+                  <p className=" mb:w-[300px] w-[100vw] mb-0 ml-10">
                     Join 60,000+ product managers receiving monthly updates on:{" "}
                   </p>
                   <img
                     src="/images/tick.svg"
-                    className="relative top-6 ml-4"
+                    className="relative md:top-6  top-11 md:ml-4  ml-10 md:mt-0 "
                     alt=""
                   />{" "}
-                  <p className="ml-11">
+                  <p className="mb-3 lg:mb-0 md:ml-11 p-5 lg:p-0 ml-12 ">
                     {" "}
                     Product discovery and building what matters
                   </p>
                   <img
                     src="/images/tick.svg"
-                    className="relative top-6 ml-4"
+                    className="relative md:top-6  top-11 md:ml-4  ml-10 md:mt-0"
                     alt=""
                   />{" "}
-                  <p className="ml-11">
+                  <p className="mb-3 lg:mb-0 md:ml-11 p-5 lg:p-0 ml-12 ">
                     {" "}
                     Measuring to ensure updates are a successl
                   </p>
@@ -68,27 +71,29 @@ function App() {
                     className="relative top-6 ml-4"
                     alt=""
                   />{" "}
-                  <p className="ml-11 mb-5">And much more!</p>
+                  <p className="ml-11 mb-10 lg:0">And much more!</p>
                 </div>
 
                 {/* div for form ----> starts  */}
                 <div>
                   <form onSubmit={(e) => changeStatusOfIsTrue(e)}>
-                    <label htmlFor="email" className="ml-5 font-semibold ">
+                    <label htmlFor="email" className="ml-5  font-semibold ">
                       Email address
                     </label>
                     <br />
                     <input
                       type="email"
+                      placeholder="Enter your email"
                       required
-                      className="border-2 rounded-lg border-black ml-5  px-20 mt-2 py-3"
+                      className="border-2   mb-5 lg:mb-0 rounded-lg border-black ml-5  px-16 lg:px-20 mt-2 py-3"
                       name=""
                       id="email"
+
                     />
                     <br />
                     <button
                       type="submit"
-                      className="ml-5 text-white font-semibold bg-[#35384B] mt-3 px-[55px] py-3 border-2 border-black rounded-lg"
+                      className="ml-5 relative bottom-5 lg:bottom-0 text-white font-semibold bg-[#35384B] mt-3   px-[40px] lg:px-[55px] py-3 border-2 border-black rounded-lg"
                     >
                       Subscrbe to monthly newsletter
                     </button>
@@ -99,7 +104,7 @@ function App() {
 
               {/* second div for img  */}
             
-              <div className="px-5 py-4 ">
+              <div className="px-5 py-4 hidden lg:block ">
                 <img
                   src="/images/illustration.svg"
                   className="hidden lg:block"
